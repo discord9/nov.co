156ead6ca8a2a094f790c9a1fb699c7ad5154ec2
discord9 <discord9@163.com>
2022-04-14 23:16:29 +0800

    git rm -cached *.zip *.bat

--- copy&run.bat
+++
@@ -1,7 +0,0 @@
-::复制编译好的.jar文件并打开游戏
-copy NeoUSSR.jar /y mods\UnityForce\jars\Unity_Force.jar
-copy src\data\scripts\UnityForceModPlugin.java /y mods\UnityForce\data\scripts\UnityForceModPlugin.java
-xcopy mods\UnityForce /E /Y D:\StarSector\ss0.9.1\Starsector\mods\UnityForce
-cd D:\StarSector\ss0.9.1\Starsector\
-starsector.exe
-cd D:\ModdingSS\NeoUSSR
--- only_copy.bat
+++
@@ -1,3 +0,0 @@
-::只复制编译好的.jar文件和ModPlugin.java文件
-copy src\data\scripts\UnityForceModPlugin.java /y mods\UnityForce\data\scripts\UnityForceModPlugin.java
-xcopy mods\UnityForce /E /Y D:\StarSector\ss0.9.1\Starsector\mods\UnityForce
